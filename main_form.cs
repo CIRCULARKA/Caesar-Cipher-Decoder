491ed7b3af895dcbdee327d949b750e51691891a
CIRCULARKA <donkardon@list.ru>
2020-05-25 23:07:55 +0500

    Added header of program and it's size

--- main_form.cs
+++ main_form.cs
@@ -1,9 +1,17 @@
 using System.Windows.Forms;
+using System.Drawing;
 
 // Class that represents main window of the program
 class MainForm : Form
 {
 	public MainForm()
 	{
+		// Fixed 300x300 pixels window
+		FormBorderStyle = FormBorderStyle.Fixed3D;
+		Size = new Size(300, 300);
+
+		// Icon for the program
+		Icon = new Icon("resources\\main_icon.ico");
+		Text = "Caesar's decryptor";
 	}
 }
