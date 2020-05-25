a93944401ccfd467e527d7f23f170817fce024a4
CIRCULARKA <donkardon@list.ru>
2020-05-25 23:33:23 +0500

    Added field for inputting

--- main_form.cs
+++ main_form.cs
@@ -6,12 +6,35 @@ class MainForm : Form
 {
 	public MainForm()
 	{
-		// Fixed 300x300 pixels window
+		//
+		// Window size
+		//
 		FormBorderStyle = FormBorderStyle.Fixed3D;
 		Size = new Size(300, 300);
 
-		// Icon for the program
+		//
+		// Header of the program
+		//
 		Icon = new Icon("resources\\main_icon.ico");
 		Text = "Caesar's decryptor";
+
+		//
+		// Objects to input
+		//
+		// Invitation label
+		Label input_label = new Label();
+		input_label.Text = "Input string to encrypt/decrypt it:";
+		input_label.Location = new Point(10, 10);
+		input_label.Size = new Size(250, 13);
+
+		Controls.Add(input_label);
+
+		// Box for inputting string
+		TextBox usr_input = new TextBox();
+		usr_input.Top = input_label.Top + 20;
+		usr_input.Left = input_label.Left;
+		usr_input.Size = new Size(this.Width - (input_label.Left * 3), 13);
+
+		Controls.Add(usr_input);
 	}
 }
