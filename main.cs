4cd6a6ac56b5ea7df15840c59b88572eabef3bb0
CIRCULARKA <donkardon@list.ru>
2020-05-25 22:31:53 +0500

    Empty form added

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows.Forms;
+
+class CaesarCiphre
+{
+	[STAThread]
+	public static void Main()
+	{
+		MainForm window = new MainForm();
+		Application.Run(window);
+	}
+}
---
+++ main_form.cs
@@ -0,0 +1,9 @@
+using System.Windows.Forms;
+
+// Class that represents main window of the program
+class MainForm : Form
+{
+	public MainForm()
+	{
+	}
+}
