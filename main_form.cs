69d25d69819372712e3c612089ba347c53da43ea
CIRCULARKA <donkardon@list.ru>
2020-05-27 15:37:09 +0500

    Added box for key input

--- main_form.cs
+++ main_form.cs
@@ -7,9 +7,9 @@ class MainForm : Form
 	public MainForm()
 	{
 		//
-		// Window size
+		// Window size and style
 		//
-		FormBorderStyle = FormBorderStyle.Fixed3D;
+		FormBorderStyle = FormBorderStyle.FixedSingle;
 		Size = new Size(300, 300);
 
 		//
@@ -22,21 +22,23 @@ class MainForm : Form
 		// Objects to input
 		//
 
-		const int VER_DIST = 25;
+		const int VER_DIST = 3;
 		const int SYMB_HEIGHT = 13;
+		Point margin = new Point(10, 10);
 
 		// Invitation label
 		Label input_label = new Label();
 		input_label.Text = "Input string to encrypt/decrypt it:";
-		input_label.Location = new Point(10, 10);
+		input_label.Location = margin;
 		input_label.Size = new Size(this.Width - (input_label.Left * 3), SYMB_HEIGHT);
 
 		Controls.Add(input_label);
 
 		// Box for inputting string
 		TextBox usr_input = new TextBox();
-		usr_input.Top = input_label.Top + VER_DIST;
-		usr_input.Left = input_label.Left;
+		usr_input.Top = input_label.Bottom + VER_DIST;
+		usr_input.Left = margin.X;
+		usr_input.BorderStyle = BorderStyle.FixedSingle;
 		usr_input.Size = new Size(this.Width - (input_label.Left * 3), SYMB_HEIGHT);
 
 		Controls.Add(usr_input);
@@ -45,14 +47,14 @@ class MainForm : Form
 		// Key presence check
 		//
 
-		const int HOR_DIST = 20;
+		const int HOR_DIST = 5;
 
 		// Checkbox for it
 		CheckBox key_check = new CheckBox();
 		key_check.Size = new Size(15, 15);
-		key_check.Left = usr_input.Left;
-		key_check.Top = usr_input.Top + VER_DIST;
-		key_check.AutoCheck = true;
+		key_check.Left = margin.X;
+		key_check.FlatStyle = FlatStyle.Flat;
+		key_check.Top = usr_input.Bottom + VER_DIST;
 
 		Controls.Add(key_check);
 
@@ -61,9 +63,27 @@ class MainForm : Form
 		int control_width = this.Width - ((key_check.Left * 2) + key_check.Width + HOR_DIST);
 		key_presence_label.Size = new Size(control_width, SYMB_HEIGHT);
 		key_presence_label.Top = key_check.Top;
-		key_presence_label.Left = key_check.Left + HOR_DIST;
+		key_presence_label.Left = key_check.Right + HOR_DIST;
 		key_presence_label.Text = "Do you have a key to decrypt with?";
 
 		Controls.Add(key_presence_label);
+
+		// Text for key input
+		Label key_input_text = new Label();
+		key_input_text.Top = key_check.Bottom + VER_DIST;
+		key_input_text.Left = margin.X;
+		key_input_text.Size = new Size(53, SYMB_HEIGHT);
+		key_input_text.Text = "Input key:";
+
+		Controls.Add(key_input_text);
+
+		// Input box for key
+		NumericUpDown key_input = new NumericUpDown();
+		key_input.Top = key_check.Bottom + VER_DIST;
+		key_input.Left = key_input_text.Right + HOR_DIST;
+		key_input.Width = this.Width - ((margin.X * 3) + key_input_text.Width + HOR_DIST);
+		key_input.BorderStyle = BorderStyle.FixedSingle;
+
+		Controls.Add(key_input);
 	}
 }
