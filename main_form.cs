bf44190b938842340cc0503164223a9d2e84ffb1
CIRCULARKA <donkardon@list.ru>
2020-05-27 19:08:12 +0500

    Some code refactoring

--- main_form.cs
+++ main_form.cs
@@ -28,6 +28,7 @@ class MainForm : Form
 		// Invitation label
 		Label input_label = new Label();
 		input_label.Location = margin;
+
 		input_label.Size = new Size(this.Width - (margin.X * 3), SYMB_HEIGHT);
 
 		input_label.Text = "Input string to encrypt/decrypt it:";
@@ -38,8 +39,9 @@ class MainForm : Form
 		TextBox usr_input = new TextBox();
 		usr_input.Top = input_label.Bottom + VER_DIST;
 		usr_input.Left = margin.X;
-		usr_input.BorderStyle = BorderStyle.FixedSingle;
+
 		usr_input.Size = new Size(this.Width - (input_label.Left * 3), SYMB_HEIGHT);
+		usr_input.BorderStyle = BorderStyle.FixedSingle;
 
 		Controls.Add(usr_input);
 
@@ -51,19 +53,22 @@ class MainForm : Form
 
 		// Checkbox for it
 		CheckBox key_check = new CheckBox();
-		key_check.Size = new Size(SYMB_HEIGHT, SYMB_HEIGHT);
-		key_check.Left = margin.X;
-		key_check.FlatStyle = FlatStyle.Flat;
 		key_check.Top = usr_input.Bottom + VER_DIST;
+		key_check.Left = margin.X;
+
+		key_check.Size = new Size(SYMB_HEIGHT, SYMB_HEIGHT);
+		key_check.FlatStyle = FlatStyle.Flat;
 
 		Controls.Add(key_check);
 
 		// Text for key presence check
 		Label key_presence_label = new Label();
-		int control_width = this.Width - ((key_check.Left * 2) + key_check.Width + HOR_DIST);
-		key_presence_label.Size = new Size(control_width, SYMB_HEIGHT);
 		key_presence_label.Top = key_check.Top;
 		key_presence_label.Left = key_check.Right + HOR_DIST;
+
+		int control_width = this.Width - ((key_check.Left * 2) + key_check.Width + HOR_DIST);
+		key_presence_label.Size = new Size(control_width, SYMB_HEIGHT);
+
 		key_presence_label.Text = "Do you have a key to decrypt with?";
 
 		Controls.Add(key_presence_label);
@@ -72,7 +77,9 @@ class MainForm : Form
 		Label key_input_text = new Label();
 		key_input_text.Top = key_check.Bottom + VER_DIST;
 		key_input_text.Left = margin.X;
+
 		key_input_text.Size = new Size(53, SYMB_HEIGHT);
+
 		key_input_text.Text = "Input key:";
 
 		Controls.Add(key_input_text);
@@ -81,6 +88,7 @@ class MainForm : Form
 		NumericUpDown key_input = new NumericUpDown();
 		key_input.Top = key_check.Bottom + VER_DIST;
 		key_input.Left = key_input_text.Right + HOR_DIST;
+
 		key_input.Width = this.Width - ((margin.X * 3) + key_input_text.Width + HOR_DIST);
 		key_input.BorderStyle = BorderStyle.FixedSingle;
 
@@ -90,17 +98,20 @@ class MainForm : Form
 
 		// Invitation to choice
 		Label action_text = new Label();
-		action_text.Left = margin.X;
 		action_text.Top = key_input.Bottom + VER_DIST;
+		action_text.Left = margin.X;
+
 		action_text.Size = new Size(120, SYMB_HEIGHT);
+
 		action_text.Text = "What you want to do?:";
 
 		Controls.Add(action_text);
 
 		// Action choice
 		ComboBox actions = new ComboBox();
-		actions.Left = action_text.Right + HOR_DIST;
 		actions.Top = action_text.Top;
+		actions.Left = action_text.Right + HOR_DIST;
+
 		control_width = this.Width - ((margin.X * 3) + action_text.Width + HOR_DIST);
 		actions.Size = new Size(control_width, SYMB_HEIGHT);
 		actions.DropDownStyle = ComboBoxStyle.DropDownList;
@@ -119,8 +130,10 @@ class MainForm : Form
 		Label result_text = new Label();
 		result_text.Top = action_text.Bottom + VER_DIST;
 		result_text.Left = margin.X;
+
 		control_width = this.Width - (margin.X * 3);
 		result_text.Size = new Size(control_width, SYMB_HEIGHT);
+
 		result_text.Text = "Result:";
 
 		Controls.Add(result_text);
@@ -129,6 +142,7 @@ class MainForm : Form
 		TextBox result = new TextBox();
 		result.Top = result_text.Bottom + VER_DIST;
 		result.Left = margin.X;
+
 		result.Width = this.Width - (margin.X * 3);
 		result.ReadOnly = true;
 		result.BorderStyle = BorderStyle.FixedSingle;
@@ -146,7 +160,6 @@ class MainForm : Form
 
 		next_key.Left = margin.X;
 		next_key.Top = result.Bottom + VER_DIST;
-
 
 	}
 }
